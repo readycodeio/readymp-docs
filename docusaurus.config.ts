9aac71fb4d24a87c9c892f08135613c5bd187415
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-03-25 14:32:41 +0100

    Add internationalization support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zh-Hans']
   },
 
   presets: [
